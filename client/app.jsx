8ea36ec1a531306c8dc4f2db879788ac0a84a367
nick_d <ngdaniele@gmail.com>
2016-11-27 16:21:23 -0800

    Changed client/app.js to jsx file

---
+++ client/app.jsx
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const App = () => (
+  <h1>{'This W'}</h1>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
