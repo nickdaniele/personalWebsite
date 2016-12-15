a519fd491a837d8be057f052167f949c84e4fae3
nick_d <ngdaniele@gmail.com>
2016-12-14 23:59:40 -0800

    Final Commit for Day

--- client/app.jsx
+++ client/app.jsx
@@ -5,6 +5,7 @@ import Introduction from './components/introduction/introduction';
 import Projects from './components/projects/projects';
 import Bookshelf from './components/bookshelf/bookshelf';
 import Contact from './components/contact/contact';
+
 import Bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css';
 import MainCSS from './main.css';
 
