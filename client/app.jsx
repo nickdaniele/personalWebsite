4890130da6e71b6439743a96b0a2a72ae1bc759c
nick_d <ngdaniele@gmail.com>
2016-12-14 21:32:41 -0800

    Added base components for each section of app

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,17 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Introduction from './components/introduction/introduction';
+import Projects from './components/projects/projects';
+import Bookshelf from './components/bookshelf/bookshelf';
+import Contact from './components/contact/contact';
 
 const App = () => (
-  <h1>{'This W'}</h1>
+  <div>
+    <Introduction />
+    <Projects />
+    <Bookshelf />
+    <Contact />
+  </div>
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
